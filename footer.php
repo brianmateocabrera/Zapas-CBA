5ccff842ce8d8f668d5f0c3cd0cc8f72d1d16ac9
Brian Cabrera <57054546+brianmateocabrera@users.noreply.github.com>
2025-05-09 19:46:11 -0300

    Crear footer.php

---
+++ footer.php
@@ -0,0 +1,20 @@
+  <!-- Pie de página con enlaces -->
+<footer class="text-center py-4">
+  <p class="mb-1">© 2025 Catálogo web creado por Brian Cabrera</p>
+
+  <!-- Íconos sociales -->
+  <div class="d-flex justify-content-center gap-4 mt-0">
+    <a href="https://wa.me/5493516618737?text=Hola%20estoy%20interesado%20en%20contratar%20tus%20servicios%20para%20crear%20una%20p%C3%A1gina%20web.%20%C2%BFPodr%C3%ADas%20enviarme%20informaci%C3%B3n%20por%20favor%3F" target="_blank" class="text-dark fs-3">
+  <i class="fab fa-whatsapp"></i>
+</a>
+    <a href="mailto:brianmateocabrera@gmail.com" target="_blank" class="text-dark fs-3">
+      <i class="fas fa-envelope"></i>
+    </a>
+    <a href="https://www.facebook.com/" target="_blank" class="text-dark fs-3">
+      <i class="fab fa-facebook"></i>
+    </a>
+    <a href="https://www.instagram.com/" target="_blank" class="text-dark fs-3">
+      <i class="fab fa-instagram"></i>
+    </a>
+  </div>
+</footer>
